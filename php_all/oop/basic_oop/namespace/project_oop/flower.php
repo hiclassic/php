9d12f13c9da565938c46dcaa78560d369b8ab243
hiclassic <hares.ahmed.319@gmail.com>
2025-07-01 20:46:46 +0600

    flower

---
+++ php_all/oop/basic_oop/namespace/project_oop/flower.php
@@ -0,0 +1,18 @@
+
+<?php
+namespace Flowr;
+
+class Flower {
+  // Properties
+  public $name="bmw";
+  public $color="red";
+
+  // Methods
+ public function show($name) {
+    $this->name = $name;
+    return $this->name;
+  }
+}
+
+
+?>
---
+++ php_all/oop/basic_oop/namespace/user.php
@@ -0,0 +1,14 @@
+
+<?php 
+namespace Uapp;
+class User{ 
+    public $name;
+    public $age;
+
+    public function show () { 
+        echo "this next page";
+    }
+}
+
+?>
+    
