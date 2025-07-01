d6a7272261eca8970b6dbd77d0247486052e5a72
hiclassic <hares.ahmed.319@gmail.com>
2025-07-01 20:48:35 +0600

    new

--- php_all/oop/basic_oop/namespace/project_oop/fruits.php
+++ php_all/oop/basic_oop/namespace/project_oop/fruits.php
@@ -16,3 +16,18 @@ class Flower {
 
 
 ?>
+<?php
+require 'fruits.php';
+require 'flower.php';
+
+use Flowr\Flower;
+use Fruits\Fruit;
+$banana = new Fruit();
+echo $banana->name;
+echo "<br>";
+echo $banana->info('sdsa');
+
+$flowr =new Flower();
+echo $flowr-> show("Rose");
+
+?>
